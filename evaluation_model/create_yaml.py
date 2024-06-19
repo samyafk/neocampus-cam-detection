8635cec0b3facbe74fd56c6be5a678ea8328efad
WantWiz <39158853+WantWiz@users.noreply.github.com>
2024-06-19 16:13:36 +0200

    d

--- evaluation_model/create_yaml.py
+++ evaluation_model/create_yaml.py
@@ -18,9 +18,9 @@ def create_yaml_files(base_path, brightness_values, contrast_values, class_names
             
             yaml_content = {
                 'path': dataset_path,
-                'train': '/images',  # Assuming we use the 'images' folder for validation as well
-                'val': '/images',
-                'test': '/images',
+                'train': './images',  # Assuming we use the 'images' folder for validation as well
+                'val': './images',
+                'test': './images',
                 'nc': len(class_names),
                 'names': class_names
             }
