c1351a9fb9831168569e5283d0b769394e865f98
WantWiz <39158853+WantWiz@users.noreply.github.com>
2024-06-19 11:14:22 +0200

    c

--- evaluation_model/create_yaml.py
+++ evaluation_model/create_yaml.py
@@ -5,6 +5,16 @@ output_base_path = "/usr/src/ultralytics/videos/datasets/data_modified"
 brightness_values = [-50, 0, 50]
 contrast_values = [0.5, 1.0, 1.5]
 
+class CustomDumper(yaml.SafeDumper):
+    pass
+
+def str_presenter(dumper, data):
+    if '\n' in data:  # if the string contains newlines, use the block format
+        return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='|')
+    return dumper.represent_scalar('tag:yaml.org,2002:str', data)
+
+CustomDumper.add_representer(str, str_presenter)
+
 # Créer les fichiers YAML de configuration
 def create_yaml_files(base_path, brightness_values, contrast_values):
     for brightness in brightness_values:
@@ -23,7 +33,7 @@ def create_yaml_files(base_path, brightness_values, contrast_values):
             
             yaml_file = os.path.join(dataset_path, f"{output_folder}.yaml")
             with open(yaml_file, 'w') as f:
-                yaml.dump(yaml_content, f, default_flow_style=None, sort_keys=False)
+                yaml.dump(yaml_content, f, Dumper=CustomDumper, default_flow_style=None, sort_keys=False)
 
 # Créer les fichiers YAML pour chaque ensemble d'images modifiées
 create_yaml_files(output_base_path, brightness_values, contrast_values)
